30e1f69263a419c6ff730a1b1c4a88d0f139a3c7
NightRaven <53287573+Xynnix@users.noreply.github.com>
2021-11-19 22:45:50 -0500

    Update TicketListenerPlugin.js

--- listeners/TicketListenerPlugin.js
+++ listeners/TicketListenerPlugin.js
@@ -1,6 +1,6 @@
 /*!
- * @plugin bastion-example-plugin
- * @author Sankarsan Kampa (k3rn31p4nic)
+ * @plugin bastion-ticket-plugin
+ * @author Xynnix (NightRaven)
  * @license MIT
  */
 
@@ -16,38 +16,44 @@ class TicketListenerPlugin extends tesseract.Listener {
     }
 
     exec = async (reaction, user) => {
-        if (reaction.name !== "🎫") return;
-        schema.find({}, function (err, docs) {
+	if (reaction.emoji.name != "🎫") return;
+			console.log("1")
+        schema.find({
+		guildID: reaction.message.guild.id
+		}, function (err, docs) {
             if (err) return console.log(err)
             docs.forEach(async doc => {
-                if (doc.guildId == reaction.message.guild.id) {
-                    const roles = await mreaction.guild.roles.cache.filter(r => { r.permissions.has("MANAGE_CHANNELS") && r.permissions.has("ADMINISTRATOR") }).first()
+			console.log("2")
+                    const roles = await reaction.message.guild.roles.cache.filter(r => { r.permissions.has("MANAGE_CHANNELS") && r.permissions.has("ADMINISTRATOR") }).first()
                     const cat = await reaction.message.guild.channels.cache.get(doc.ticketCategory);
                     const chan = await reaction.message.guild.channels.cache.get(doc.ticketChannel);
-                    const msg = await chan.messages.fetch(docs.messageId);
-                    if (reaction.message.id == msg.id) {
-                        button.guild.channels.create(`ticket-${count}`, {
+                    const msg = doc.messageId
+		    if(reaction.message.guild.channels.cache.find(n => n.name == `ticket-${user.id}`)) return user.send("You already have a ticket in that server!");
+                    if (reaction.message.id == msg) {
+			console.log("3")
+                        reaction.message.guild.channels.create(`ticket-${user.id}`, {
                             permissionOverwrites: [
                                 {
                                     id: reaction.message.guild.id,
                                     deny: ['VIEW_CHANNEL'],
                                 },
                                 {
-                                    id: roles.id || message,
+                                    id: roles || reaction.client.user.id,
                                     allow: ['VIEW_CHANNEL', `READ_MESSAGE_HISTORY`, `ATTACH_FILES`, `SEND_MESSAGES`],
                                 },
                                 {
                                     id: user.id,
                                     allow: ['VIEW_CHANNEL', `READ_MESSAGE_HISTORY`, `ATTACH_FILES`, `SEND_MESSAGES`],
                                 },
-                            ], parent: (cat), position: 1, topic: `A Ticket : <@!${reaction.message.user.id}>`, reason: "Ticket Created"
-                        })
+                            ], parent: (cat), position: 1, topic: `A Ticket : <@!${user.id}>`, reason: "Ticket Created"
+                        }).then(ms =>{
+			ms.send(`${user}, Please describe the reason you made this ticket and the server staff will be with you as soon as possible!`);
+		})
 
                     }
-                };
+                });
             });
-        });
+        }
     }
-}
 
 module.exports = TicketListenerPlugin;
